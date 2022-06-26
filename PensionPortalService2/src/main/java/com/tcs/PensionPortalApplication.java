c652d7753bffbd03d04279738865262bb852be11
Himanshu Arya <34008932+ohmnom@users.noreply.github.com>
2022-06-26 15:32:59 +0530

    Add files via upload

---
+++ PensionPortalService2/src/main/java/com/tcs/PensionPortalApplication.java
@@ -0,0 +1,34 @@
+package com.tcs;
+
+import java.sql.SQLException;
+
+import org.h2.tools.Server;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class PensionPortalApplication {
+	
+	@RequestMapping("/")
+	public String home() {
+		return "Service2";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(PensionPortalApplication.class, args);
+	}
+	
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	public Server inMemoryH2DatabaseaServer() throws SQLException {
+	    return Server.createTcpServer(
+	      "-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
+	}
+
+}
